659ff428541258c7bc6c88687bcd695f5393a329
hyunjae0912 <hyunjae00912@naver.com>
2024-04-30 10:25:33 +0900

    monster_ai
    대충 만듦 오류 생기면 나중에 추가 예정

--- DeepDarkLabyrinthSurvival/Assets/Script/Monster_Aitest.cs
+++ DeepDarkLabyrinthSurvival/Assets/Script/Monster_Aitest.cs
@@ -5,67 +5,68 @@ using UnityEngine;
 
 public class Monster_Aitest : MonoBehaviour
 {
-    private GameObject player;
-    public float moveSpeed = 1f;
     public float detectionRange = 10f;
-
+    public float moveSpeed = 1f;
+    //공격여부
     private bool isAttacking = false;
     private float stoptime = 3f;
-    private float timer = 0;
+    private float timer = 0f;
 
-    private void Awake()
-    {
-        player = GameObject.FindWithTag("Player");
-    }
+    private GameObject player;
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("플레이어랑 닿음");
             isAttacking = true;
+        }
+    }
 
+    private void stopmove()
+    {
+        Debug.Log("시간 증가중");
+        timer += Time.deltaTime;
+
+        //멈추는 시간 다 되면
+        if (timer >= stoptime)
+        {   //멈추는거 끝내고 시간 초기화
+            isAttacking = false;
+            timer = 0f;
+            moveSpeed = 1f;
+            Debug.Log("끝남");
+        }
+        //멈추고 있음
+        else
+        {
+            Debug.Log("멈추고 있음");
+            moveSpeed = 0f;
         }
     }
 
 
+
     private void Update()
     {
-        // 플레이어를 감지함 플레이어와 적의 사이의 거리
-        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
 
+        player = GameObject.FindWithTag("Player");
+        float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
         // 플레이어가 일정 범위 내에 있으면
         if (distanceToPlayer <= detectionRange)
         {
+            //거리 측정
             Vector3 direction = (player.transform.position - transform.position).normalized;
 
             // 플레이어를 향해 이동
-            if (isAttacking == false)
+            if (!isAttacking)
             {
                 transform.LookAt(player.transform);
                 transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
             }
 
-            //플레이어랑 닿았을 때
+
             if (isAttacking)
             {
-                Debug.Log("시간 증가중");
-                timer += Time.deltaTime;
-
-                //멈추는 시간 다 되면
-                if (timer >= stoptime)
-                {   //멈추는거 끝내고 시간 초기화
-                    isAttacking = false;
-                    timer = 0f;
-                    moveSpeed = 1f;
-                    Debug.Log("끝남");
-                }
-                //멈추고 있음
-                else
-                {
-                    Debug.Log("멈추고 있음");
-                    moveSpeed = 0f;
-                }
+                stopmove();
             }
         }
     }
