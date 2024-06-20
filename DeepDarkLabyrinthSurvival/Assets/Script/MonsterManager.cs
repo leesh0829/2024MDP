c8f30701c49a35e06c4c665ed2c259e96543d771
Adelie <lsh77392125@gmail.com>
2024-06-20 19:40:20 +0900

    Reapply "hp bar"
    This reverts commit 7f64da4ee718c06e3063c9cdc9e3143783295634.

--- DeepDarkLabyrinthSurvival/Assets/Script/MonsterManager.cs
+++ DeepDarkLabyrinthSurvival/Assets/Script/MonsterManager.cs
@@ -5,7 +5,6 @@ using UnityEngine.UI;
 
 public class MonsterManager : MonoBehaviour
 {
-    public int MonCnt = MazeSpawner.instance.MonstCNT;
     public Text MonText;
     
     // Start is called before the first frame update
@@ -17,6 +16,7 @@ public class MonsterManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        int MonCnt = MazeSpawner.instance.MonstCNT;
         int MonAtt = Gamemanager.instance.attcount;
         Debug.Log("MM에서 처치 수:" +  MonAtt);
         MonText.text = (MonCnt-MonAtt).ToString();
